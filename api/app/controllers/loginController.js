0dfb58506cee9f19d937d10137ba6ddbd9c3f1a0
Ghyath Moussa <ghyathmoussa@hotmail.com>
2022-05-13 02:19:29 +0300

    create student and modify contoller files

---
+++ api/app/controllers/loginController.js
@@ -0,0 +1,20 @@
+exports.login = async (req,res) =>{
+    console.log(req.body);
+    const { username, password} = req.body;
+    mysqlConnection.query('SELECT * FROM kayit_elemani Where eleman_username = ? AND eleman_password = ? ',[username,password], (err, result) => {
+        if (err){
+            console.log(err)
+            res.send({err: err})
+        }
+        else{
+            if(result){
+                res.send(result)
+            }
+            else{
+                res.send({message: "No user found"})
+            }
+        }
+            
+    })
+}
+
--- api/app/routes/route.js
+++ api/app/routes/route.js
@@ -1,30 +1,13 @@
 const express = require('express');
 const mysqlConnection = require("../config/db_config");
 const router = express.Router();
+const loginC = require('../controllers/loginController')
 
 router.get('/',(req,res) => {
     res.status(200).json({message:"Hello"})
 })
 
-router.post('/login', async (req,res) =>{
-    console.log(req.body);
-    const { username, password} = req.body;
-    mysqlConnection.query('SELECT * FROM kayit_elemani Where eleman_username = ? AND eleman_password = ? ',[username,password], (err, result) => {
-        if (err){
-            console.log(err)
-            res.send({err: err})
-        }
-        else{
-            if(result){
-                res.send(result)
-            }
-            else{
-                res.send({message: "No user found"})
-            }
-        }
-            
-    })
-})
+router.post('/login', loginC.login)
 
 
 
