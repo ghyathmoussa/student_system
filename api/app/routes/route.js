796cd146e8fd81d895c70317efacb5a26d032c73
Ghyath Moussa <ghyathmoussa@hotmail.com>
2022-05-13 02:23:03 +0300

    student register

--- api/app/routes/route.js
+++ api/app/routes/route.js
@@ -2,12 +2,14 @@ const express = require('express');
 const mysqlConnection = require("../config/db_config");
 const router = express.Router();
 const loginC = require('../controllers/loginController')
+const studentC = require('../controllers/studentController')
 
 router.get('/',(req,res) => {
     res.status(200).json({message:"Hello"})
 })
 
 router.post('/login', loginC.login)
+router.post('/register-student',studentC.createStudents)
 
 
 
