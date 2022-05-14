7b6acb227759d28f5d070d9acce4761ff1a870ea
Ghyath Moussa <ghyathmoussa@hotmail.com>
2022-05-14 14:00:28 +0300

    modify links

--- api/app/routes/route.js
+++ api/app/routes/route.js
@@ -1,13 +1,10 @@
 const express = require('express');
-const mysqlConnection = require("../config/db_config");
+// const mysqlConnection = require("../config/db_config");
 const router = express.Router();
 const loginC = require('../controllers/loginController')
 const studentC = require('../controllers/studentController')
 
-router.get('/',(req,res) => {
-    res.status(200).json({message:"Hello"})
-})
-
+// api links
 router.post('/auth/login', loginC.login)
 router.post('/register-student',studentC.createStudents)
 router.get('/show-students',studentC.showStudents)
